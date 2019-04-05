0b296b9d6b6ac21fa88d90aa6e59be8a08d45e7c
h2002d <davit.harutyunyan1996@gmail.com>
2019-04-05 22:22:12 +0400

    Initializing files

---
+++ Hirent/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Hirent.Models;
+namespace Hirent.Controllers
+{
+    public class HomeController : BaseController
+    {
+        public ActionResult Index()
+        {
+            ViewBag.ParentCategories = Models.ParentCategory.GetCategory(null,false);
+            ViewBag.MainCategories = Models.Category.GetCategory(null,false).Count>4? Models.Category.GetCategory(null,false).Take(4): Models.Category.GetCategory(null,false);
+            ViewBag.MainProducts = Models.Product.GetProduct(null,false).Count>12? Models.Product.GetProduct(null, false).Take(12): Models.Product.GetProduct(null, false);
+            return PartialView();
+        }
+
+        public ActionResult Category(int id)
+        {
+            ViewBag.ParentCategories = Models.ParentCategory.GetCategory(null,false);
+            ViewBag.CategoryName = Models.Category.GetCategory(id,false).First().Name;
+            var products = Models.Product.GetProductByCategoryId(id, false);
+            return PartialView(products);
+        }
+        public ActionResult Search(string query)
+        {
+            var product = Product.GetProductByQuery(query, false);
+            ViewBag.ParentCategories = Models.ParentCategory.GetCategory(null, false);
+            ViewBag.CategoryName = string.Format(@"Search results for ""{0}""", query);
+            return PartialView("Category", product);
+        }
+        public ActionResult About()
+        {
+            ViewBag.ParentCategories = Models.ParentCategory.GetCategory(null, false);
+            return PartialView();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.ParentCategories = Models.ParentCategory.GetCategory(null, false);
+            return PartialView();
+        }
+
+        public ActionResult ProductPartial(int id)
+        {
+            var product = Product.GetProduct(id, false).First();
+            return PartialView(product);
+        }
+        public ActionResult ChangeLanguage(string lang)
+        {
+            new SiteLanguages().SetLanguage(lang);
+            return Redirect(Request.UrlReferrer.ToString());
+        }
+    }
+}
---
+++ Hirent/Models/Product.cs
@@ -0,0 +1,55 @@
+﻿using Hirent.DAO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Hirent.Models
+{
+    public class Product
+    {
+        static ProductDAO DAO = new ProductDAO();
+        public int? Id { get; set; }
+        public string Name { get; set; }
+        public string ImageSource { get; set; }
+        public string ImageSource1 { get; set; }
+        public string ImageSource2 { get; set; }
+
+        public int CategoryId { get; set; }
+        public decimal Price { get; set; }
+
+        [DataType(DataType.Upload)]
+        public HttpPostedFileBase File { get; set; }
+
+        [DataType(DataType.Upload)]
+        public HttpPostedFileBase File1 { get; set; }
+
+        [DataType(DataType.Upload)]
+        public HttpPostedFileBase File2 { get; set; }
+        public void Save()
+        {
+            DAO.saveProduct(this);
+        }
+
+        public static List<Product> GetProduct(int? id, bool forAdmin)
+        {
+            return DAO.GetProducts(id, forAdmin);
+        }
+
+        public static List<Product> GetProductByCategoryId(int id, bool forAdmin)
+        {
+            return DAO.GetProductsByCategoryId(id, forAdmin);
+        }
+
+        public static void Delete(int id)
+        {
+            DAO.deleteProduct(id);
+        }
+
+        internal static object GetProductByQuery(string query, bool forAdmin)
+        {
+            return DAO.GetProductsByQuery(query, forAdmin);
+        }
+    }
+}
